ef57a410ce129cd2eca805221f52257b677199c5
SamSantos1jz <samsantos1jz@gmail.com>
2024-12-09 16:22:56 -0300

    first commit

---
+++ login/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def register_user(request):
+    return render(request, 'register.html')
+
+def login_user(request):
+    return render(request, 'login.html')
