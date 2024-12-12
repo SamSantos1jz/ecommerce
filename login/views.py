a1d148ef0c343844addc9452e74d2e18058c1b67
SamSantos1jz <samsantos1jz@gmail.com>
2024-12-12 11:04:14 -0300

    remove app login

--- login/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def register_user(request):
-    return render(request, 'register.html')
-
-def login_user(request):
-    return render(request, 'login.html')
